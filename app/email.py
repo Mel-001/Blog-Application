0190f036ebe82bc7bdef91b4d4cfbc9aa70c258c
Meloni <meloni.langat@student.moringaschool.com>
2022-03-15 14:19:58 +0300

    add files

---
+++ app/email.py
@@ -0,0 +1,17 @@
+import os
+from flask_mail import Message
+from flask import render_template
+from . import mail
+import smtplib
+
+
+subject_pref = 'bloGg'
+
+
+def mail_message(subject,template,to,**kwargs):
+   
+    sender_email = 'antony.akono@student.moringaschool.com'
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body= render_template(template + ".txt",**kwargs)
+    email.html = render_template(template + ".html",**kwargs)
+    mail.send(email)
